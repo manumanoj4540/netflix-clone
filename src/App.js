f7485d8efe9d3893bfe66e88ea14b4f97a50b2dc
Manu Manoj <manumanoj4540@gmail.com>
2020-07-11 16:29:09 +0530

    Code cleanup and added row components along with axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Row from './Row'
+import requests from "./requests";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Lets Gooo
+      <Row  title= "Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row  title= "Trending Now" fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }
