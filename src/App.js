53535b1d81d1b51b131e2bf1d37659dbd7dfc441
Manu Manoj <manumanoj4540@gmail.com>
2020-07-13 01:20:03 +0530

    Added navbar and style updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Row from './Row'
 import requests from "./requests";
 import Banner from './Banner'
+import Nav from './Nav'
 import './App.css';
+
 
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Banner />
       <Row  title= "Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row  title= "Trending Now" fetchUrl={requests.fetchTrending}/>
